c51ff88b8a6095613a1f56016744645239c6bcb7
iTushka <ts.hossain@gmail.com>
2026-07-27 12:19:20 +0200

    Remove forced-camera capture attribute from Image field, allow gallery selection

--- src/components/AddSheet.tsx
+++ src/components/AddSheet.tsx
@@ -607,7 +607,6 @@ export function AddSheet({
                   ref={cardFileInputRef}
                   type="file"
                   accept="image/*"
-                  capture="environment"
                   className="hidden"
                   onChange={(e) => {
                     const file = e.target.files?.[0];
